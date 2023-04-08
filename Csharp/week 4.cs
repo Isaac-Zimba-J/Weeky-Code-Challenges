cd8b263966ab5b0c9e7920c6c8353ca4e96f0814
Zimba <74758329+Isaac-Zimba-J@users.noreply.github.com>
2023-04-08 14:28:27 +0200

    Add files via upload

---
+++ Csharp/week 4.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System;
+public class ReverseWords {
+
+	public static void Main()
+	{
+		string[] s = "Heres a sentence to split".Split(' ');
+		string ans = "";
+		for (int i = s.Length - 1; i >= 0; i--) {
+			ans += s[i] + " ";
+		}
+		
+		Console.Write(ans.Substring(0, ans.Length - 1));
+	}
+}
